558c967331c3bbb31f3c9dfc88f9853c304daafc
Abhirami1303 <abhiramivs25549@gmail.com>
2024-04-11 22:26:13 +0530

    apr 11 2

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   //static String uri = 'http://localhost:3000';
-  //static String uri = 'http://103.160.194.141:3000';
+
   static String uri = 'http://192.168.1.2:3000';
   //static String uri = 'http://localhost:59999';
 }
